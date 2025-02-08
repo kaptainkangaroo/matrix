4aa1e8a34eff0f5a27f9985cee0bcd5326855819
kaptainkangaroo <karlenchang@gmail.com>
2025-02-08 15:22:35 +0800

    +

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,7 +8,11 @@ cat /artifacts/artifacts_content.log
 ls / > root_content.log
 cat root_content.log
 
-DYNAMIC_DIR=$(ls -d /artifacts/* | grep -v artifacts_content.log)
+DYNAMIC_DIR=$(find /artifacts -maxdepth 1 -type d ! -name artifacts -prune)
+if [ -z "$DYNAMIC_DIR" ]; then
+  echo "Error: Could not find dynamic directory in /artifacts"
+  exit 1
+fi
 COMPOSE_FILE_PATH="$DYNAMIC_DIR/docker-compose.yml"
 
 apk add --update nodejs npm
