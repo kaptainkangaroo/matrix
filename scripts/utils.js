cedf537e8570882b97e05640ef7dadd3a37b179e
kaptainkangaroo <karlenchang@gmail.com>
2025-02-08 14:34:57 +0800

    added log to utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,7 @@
 function getEnv(key) {
   const value = process.env[key];
   if (value === undefined) {
+    console.log("Environment variable SYNAPSE_FQDN:", process.env.SYNAPSE_FQDN);
     throw new Error(`Environment variable ${key} is not set`);
   }
   return value;
