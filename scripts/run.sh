e336ac165874df7df08c819dcc648c53c0746304
kaptainkangaroo <karlenchang@gmail.com>
2025-02-08 15:42:24 +0800

    hopeful

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,13 +2,16 @@
 
 env > env.log
 
-ls -l "$DYNAMIC_DIR" > /artifacts/artifacts_content.log
-cat /artifacts/artifacts_content.log
-
 ls / > root_content.log
 cat root_content.log
 
-DYNAMIC_DIR=$(find /artifacts -maxdepth 1 -type d ! -name artifacts -prune)
+apk add --update nodejs npm
+
+npm install dotenv
+
+node ./scripts/index.js
+
+DYNAMIC_DIR=$(find /artifacts -maxdepth 1 -type d ! -name artifacts -print)
 
 if [ -z "$DYNAMIC_DIR" ]; then
   echo "Error: Could not find dynamic directory in /artifacts using find"
