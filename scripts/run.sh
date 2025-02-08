dc142d78e584e06c164e2cae389df636ad3610d4
kaptainkangaroo <karlenchang@gmail.com>
2025-02-08 16:04:47 +0800

    jhuk

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,7 @@ ls / > root_content.log
 cat root_content.log
 
 apk add --update nodejs npm docker
+
 npm install dotenv
 node ./scripts/index.js
 
@@ -19,4 +20,23 @@ else
 fi
 
 cd "$DYNAMIC_DIR"
+
+echo "--- Docker version ---"
+docker version
+echo "--- Docker Compose version ---"
+docker compose version
+echo "--- Which docker compose ---"
+which docker compose
+echo "--- ls /usr/local/bin ---"
+ls -l /usr/local/bin
+echo "--- ls /usr/bin ---"
+ls -l /usr/bin
+echo "--- PATH environment variable ---"
+echo $PATH
+echo "--- docker info ---"
+docker info
+echo "--- docker compose config ---"
+docker compose config
+
+
 docker compose build
