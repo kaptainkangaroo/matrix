91bfa78bc34c3b3d7823a8dff86b09809b80bd46
kaptainkangaroo <karlenchang@gmail.com>
2025-02-08 15:16:55 +0800

    dynamic docker path

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,8 +8,13 @@ cat /artifacts/artifacts_content.log
 ls / > root_content.log
 cat root_content.log
 
+DYNAMIC_DIR=$(ls -d /artifacts/* | grep -v artifacts_content.log)
+COMPOSE_FILE_PATH="$DYNAMIC_DIR/docker-compose.yml"
+
 apk add --update nodejs npm
 
 npm install dotenv
 
 node ./scripts/index.js
+
+docker compose --project-directory /artifacts -f "$COMPOSE_FILE_PATH" build
