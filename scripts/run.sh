34e000dbc66aa483337edf3358e6d7f8ecad0bdf
kaptainkangaroo <karlenchang@gmail.com>
2025-02-08 15:11:49 +0800

    more logs

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,9 @@ env > env.log
 ls /artifacts > /artifacts/artifacts_content.log
 cat /artifacts/artifacts_content.log
 
+ls / > root_content.log
+cat root_content.log
+
 apk add --update nodejs npm
 
 npm install dotenv
